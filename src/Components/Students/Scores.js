d8312d585a5f24c03ae988786a6e8203c82cffb4
Alwaz <alwazkazi3@gmail.com>
2021-06-27 09:51:37 -0700

    updated score component

--- src/Components/Students/Scores.js
+++ src/Components/Students/Scores.js
@@ -3,7 +3,7 @@ import React from "react";
 const Scores = ({ score }) => {
   return (
     <div className="scores__score">
-      <h3>{score}</h3>
+      <h3 style={{ color: "#1c6f33" }}>{score}</h3>
     </div>
   );
 };
